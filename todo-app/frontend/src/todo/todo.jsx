35a8f841055aff6ea2277084ad3dc3ed8f7b5024
Márcio Vasconcelos <marcinhobasspro@icloud.com>
2020-11-25 10:47:37 -0300

    if and others

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -4,13 +4,23 @@ import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+    
+    handleAdd() {
+        console.log(this)
+    }
+   
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
-                <TodoList />
+                <TodoForm handleAdd={this.handleAdd} />
+                {/* <TodoList /> */}
             </div>
         )
     }
