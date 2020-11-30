b4f80d5190baf1c5202342a09c8c7c529147a9d7
Márcio Vasconcelos <marcinhobasspro@icloud.com>
2020-11-30 09:41:16 -0300

    30-nov

--- todo-app/backend/src/config/database.js
+++ todo-app/backend/src/config/database.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-
-module.exports = mongoose.connect('mongodb://http://localhost:27017/todo-app')
+module.exports = mongoose.connect('mongodb://localhost/todo')
--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,25 +1,45 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3004/api/todos'
 
 export default class Todo extends Component {
     constructor(props) {
         super(props)
+        this.state = {description:'' , list: [] }
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this )
+
+        this.refresh()
     }
     
-    handleAdd() {
-        console.log(this)
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+        .then(resp.setState({...this.state, description:'' , list: resp.data}))
     }
+
+    handleChange(e) {
+        this.setState({...this.state , description: e.target.value})
+    }
+    handleAdd() {
+        const description = this.state.description
+        axios.post(URL, {description})
+        .then(resp => this.refresh())
+    }
+
    
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd} />
+                <TodoForm 
+                description = {this.state.description}
+                handleAdd={this.handleAdd} 
+                handleChange={this.handleChange}/>
                 {/* <TodoList /> */}
             </div>
         )
